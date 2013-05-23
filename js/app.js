bdf353509593d5b549b701599e5ce6cc47cc6506
Micah Ransdell <mjr578@gmail.com>
2013-05-22 22:57:22 -0700

    Add bootstrap and a bootswatch theme, also setup events index

--- js/app.js
+++ js/app.js
@@ -9,17 +9,27 @@ App.Store = DS.Store.extend({
 
 App.Router.map(function() {
   this.resource("events");
-  this.resource("event", {path: "/event/:event_id"}, function(){
+  this.resource("event", {path: "/events/:event_id"}, function(){
     this.route("edit");
   });
 });
 
 App.Event = DS.Firebase.Model.extend({
   name: DS.attr("string"),
-  date: DS.attr("date")
+  date: DS.attr("date"),
+
+  formattedDate: function(){
+    return moment(this.get("date")).format("MMM Do YYYY h:mm:ss a")
+  }.property("date")
 });
 
 App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return App.Event.find();
+  }
+});
+
+App.EventsRoute = Ember.Route.extend({
   model: function() {
     return App.Event.find();
   }
